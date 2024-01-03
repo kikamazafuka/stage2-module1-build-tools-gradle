1414f0c5de3417cc42841eef8e40fdac56daa127
Artur Semenas <uncelfuka@gmail.com>
2024-01-03 23:23:07 +0100

    Stage 2 gradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,12 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        for(String s : args){
+            int n = Integer.parseInt(s);
+            if (n<0){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,8 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+
+        int num = Integer.parseInt(str);
+        return num>0;
     }
 }
